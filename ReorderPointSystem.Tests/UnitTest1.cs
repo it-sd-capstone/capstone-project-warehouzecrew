63773b5ea98433a3c69aea22f46643b8ce4bf96c
Alan Ortiz <aortizcabrero@student.cvtc.edu>
2025-11-09 12:36:59 -0600

    Add sample test example file

--- ReorderPointSystem.Tests/UnitTest1.cs
+++ ReorderPointSystem.Tests/UnitTest1.cs
@@ -2,10 +2,11 @@ namespace ReorderPointSystem.Tests
 {
     public class UnitTest1
     {
+        // Simple example of how tests work. This class can be deleted once real tests begin to be implemented
         [Fact]
         public void Test1()
         {
-
+            Assert.True(4 > 0);
         }
     }
 }
