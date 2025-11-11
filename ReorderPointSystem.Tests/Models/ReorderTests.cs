5ee1cbc7591a7604cb8bef910fe8dc40ace36594
Alan Ortiz <aortizcabrero@student.cvtc.edu>
2025-11-11 15:20:51 -0600

    Implement ReorderTests

--- ReorderPointSystem.Tests/Models/ReorderTests.cs
+++ ReorderPointSystem.Tests/Models/ReorderTests.cs
@@ -1,6 +1,33 @@
-﻿namespace ReorderPointSystem.Tests.Models
+﻿using ReorderPointSystem.Models;
+
+namespace ReorderPointSystem.Tests.Models
 {
     public class ReorderTests
     {
+        [Fact]
+        public void MarkInProcess()
+        {
+            // Arrange
+            Reorder newOrder = new Reorder(1, 1, 10);
+
+            // Act
+            newOrder.MarkInProcess();
+
+            // Assert
+            Assert.Equal("In process", newOrder.Status);
+        }
+
+        [Fact]
+        public void MarkCompleted()
+        {
+            // Arrange
+            Reorder newOrder = new Reorder(1, 1, 10);
+
+            // Act
+            newOrder.MarkCompleted();
+
+            // Assert
+            Assert.Equal("Complete", newOrder.Status);
+        }
     }
 }
