165943473c90c7f38b2fde5d55d3392724b91847
Alan Ortiz <aortizcabrero@student.cvtc.edu>
2025-11-11 15:21:32 -0600

    Implement empty test methods for ReorderRepository

--- ReorderPointSystem.Tests/Data/ReorderRepositoryTests.cs
+++ ReorderPointSystem.Tests/Data/ReorderRepositoryTests.cs
@@ -2,5 +2,60 @@
 {
     public class ReorderRepositoryTests
     {
+        private const string TestDbFilePath = "test_rps.db";
+
+        [Fact]
+        public void Add_ShouldReturnEntryId()
+        {
+
+        }
+
+        [Fact]
+        public void Update_ShouldReturnTrue_WhenEntryDoesExist()
+        {
+
+        }
+
+        [Fact]
+        public void Update_ShouldReturnFalse_WhenEntryDoesNotExist()
+        {
+
+        }
+
+        [Fact]
+        public void Delete_ShouldReturnTrue_WhenEntryDoesExist()
+        {
+
+        }
+
+        [Fact]
+        public void Delete_ShouldReturnFalse_WhenEntryDoesNotExist()
+        {
+
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnReorder_WhenEntryDoesExist()
+        {
+
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnNull_WhenEntryDoesNotExist()
+        {
+
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnEmptyList_WhenAnyEntryDoesNotExist()
+        {
+
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnReorderList_WhenAnyEntryDoesExist()
+        {
+
+        }
     }
 }
