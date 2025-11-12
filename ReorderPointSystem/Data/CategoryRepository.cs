0fc7163a22821e9222eceb8c716acd924ae1c3af
Zuayuah Xiong <zxiong4@student.cvtc.edu>
2025-11-12 13:08:53 -0600

    Create CategoryRepository Class

--- ReorderPointSystem/Data/CategoryRepository.cs
+++ ReorderPointSystem/Data/CategoryRepository.cs
@@ -10,6 +10,44 @@ namespace ReorderPointSystem.Data
 {
     internal class CategoryRepository
     {
+        private readonly List<Category> _categories = new List<Category>();
 
+        public List<Category> GetAll()
+        {
+            return _categories;
+        }
+
+        public Category? GetById(int id)
+        {
+            return _categories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public int Add(Category category)
+        {
+            if (category.Id == 0)
+                category.Id = _categories.Count > 0 ? _categories.Max(c => c.Id) + 1 : 1;
+
+            _categories.Add(category);
+            return category.Id;
+        }
+
+        public void Update(Category category)
+        {
+            var existing = GetById(category.Id);
+            if (existing != null)
+            {
+                existing.Name = category.Name;
+            }
+        }
+
+        public void Delete(int categoryId)
+        {
+            var category = GetById(categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
     }
 }
+
