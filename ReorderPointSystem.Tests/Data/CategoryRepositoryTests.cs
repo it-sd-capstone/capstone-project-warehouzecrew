77dcb0653cdce6d7de960bfbc40c44d14cf33350
Zuayuah Xiong <zxiong4@student.cvtc.edu>
2025-11-12 11:44:52 -0600

    Create Category Class

--- ReorderPointSystem.Tests/Data/CategoryRepositoryTests.cs
+++ ReorderPointSystem.Tests/Data/CategoryRepositoryTests.cs
@@ -1,6 +1,17 @@
-﻿namespace ReorderPointSystem.Tests.Data
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReorderPointSystem.Models;
+using ReorderPointSystem.Data;
+using Xunit.Abstractions;
+
+namespace ReorderPointSystem.Tests.Data
 {
     public class CategoryRepositoryTests
-    {
+    { 
+
     }
 }
+
--- ReorderPointSystem/Data/CategoryRepository.cs
+++ ReorderPointSystem/Data/CategoryRepository.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using ReorderPointSystem.Models;
 
 namespace ReorderPointSystem.Data
 {
     internal class CategoryRepository
     {
+
     }
 }
