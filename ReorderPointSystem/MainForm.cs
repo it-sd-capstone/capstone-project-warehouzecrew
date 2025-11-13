3c1d37223f8e85bcafbfea856bf9f87547a0d65f
Zachary Vaughn <zvaughn1@student.cvtc.edu>
2025-11-13 13:44:08 -0600

    Add comments for event explainations

--- ReorderPointSystem/MainForm.cs
+++ ReorderPointSystem/MainForm.cs
@@ -5,21 +5,30 @@ namespace ReorderPointSystem
 {
     public partial class MainForm : Form
     {
-        private void DisableProductInfoOptions()
-        {
-            ItemInfoGroupBox.Enabled = false;
-        }
-
-        private void EnableProductInfoOptions()
-        {
-            ItemInfoGroupBox.Enabled = true;
-        }
-
         public MainForm()
         {
             InitializeComponent();
         }
 
+        // Helper function to disable editing item information
+        private void DisableProductInfoOptions()
+        {
+            ItemInfoGroupBox.Enabled = false;
+        }
+
+        // Helper function to enable editing item information
+        private void EnableProductInfoOptions()
+        {
+            ItemInfoGroupBox.Enabled = true;
+        }
+
+        // Form load events, all will happen before the form displays to the user
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Database.Initialize();
+        }
+
+        // Display or hide the Simulation buttons
         private void EnableTestModeChkbx_CheckedChanged(object sender, EventArgs e)
         {
             if (EnableTestModeChkbx.Checked == true)
@@ -34,57 +43,63 @@ namespace ReorderPointSystem
             }
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-            Database.Initialize();
-            DisableProductInfoOptions();
-        }
-
+        // When the simulate day button is pressed, each item in the DB has a chance to deplete a random amount of stock
         private void SimDayBtn_Click(object sender, EventArgs e)
         {
-
+            
         }
 
+        // Insert dummy records into the DB for testing purposes
         private void AddTestDataBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // IF the item displayed is already in the DB, update the record for said item
+        // ELSE create a new item in the BD with the properties set by the user
         private void SubmitItemBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Clear the item information in the item info group box. If the text boxes are disabled, also enable them
         private void ClearFieldsBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Remove the selected item from the Items DB, If an item is selected
         private void DeleteItemBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Filter the items listed in the ItemsListBox box based on the text in the ItemSearchTextBox
         private void SearchBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Enables the controls in the ItemInfoGroupBox for editing
         private void EditItemBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Add the highlighted item to a pending order. If no pending orders exist, also create a new pending order
         private void AddToOrderBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Remove the pending order from the PendingOrdersListBox
+        // WILL NOT STOP THE ORDER FROM BEING RECREATED IF AN ITEM IS BELOW REORDER THRESHOLD
         private void DeletePendingOrderBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        // Edit the amount of an item to be ordered, from within a pending order
         private void EditOrderAmtBtn_Click(object sender, EventArgs e)
         {
 
