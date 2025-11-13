fb4b810f17663d415a64ffd594db09ca1454de29
Zachary Vaughn <zvaughn1@student.cvtc.edu>
2025-11-13 16:17:40 -0600

    Add functionality for the AddTestDataButton

--- ReorderPointSystem/MainForm.cs
+++ ReorderPointSystem/MainForm.cs
@@ -1,3 +1,4 @@
+using System.Data.SQLite;
 using System.Diagnostics;
 using ReorderPointSystem.Data;
 
@@ -52,7 +53,65 @@ namespace ReorderPointSystem
         // Insert dummy records into the DB for testing purposes
         private void AddTestDataBtn_Click(object sender, EventArgs e)
         {
+            SQLiteConnection conn = Database.GetConnection();
+            String[] categories = new String[] { "Produce", "Office-Supplies", "Home-Goods", "Raw-Materials", "Electronics", "Groceries" };
 
+            // for each category, if they aren't already in the categories table, add them. 
+            foreach (String str in categories)
+            {
+                String selectCommand = "SELECT id FROM categories WHERE name = \'" + str + "\'";
+                SQLiteCommand selectCMD = new SQLiteCommand(selectCommand, conn);
+                SQLiteDataReader reader = selectCMD.ExecuteReader();
+                selectCMD.Dispose();
+                if (!reader.HasRows)
+                {
+                    String insertCommand = "INSERT INTO categories (name) VALUES (\'" + str + "\')";
+                    SQLiteCommand insertCMD = new SQLiteCommand(insertCommand, conn);
+                    Debug.WriteLine(str + " was added.");
+                    insertCMD.ExecuteNonQuery();
+                    insertCMD.Dispose();
+                }
+                else
+                {
+                    Debug.WriteLine(str + " already exists in the table.");
+                }
+                reader.Close();
+            }
+
+            int[] itemCategoryID = new int[] { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4 };
+            String[] itemNames = new String[] { "Lettuce", "Staples", "Curtains", "Plywood", "GPU", "Cheerios", "Cucumbers", "Pencils", "Paper", "Linseed-oil" };
+            String[] itemDescription = new String[] { "", "", "", "", "", "", "", "", "", "" };
+            int[] itemCurrAmt = new int[] { 10, 3000, 5, 100, 5, 27, 95, 200, 4570, 46 };
+            int[] itemReorderPoint = new int[] { 5, 1000, 2, 50, 3, 20, 50, 120, 2000, 25 };
+            int[] itemReorderAmt = new int[] { 20, 4000, 8, 200, 12, 80, 200, 480, 8000, 100 };
+
+            // add sample items if they dont already exist
+            for (int i = 0; i < itemNames.Length; i++)
+            {
+                String selectCommand2 = "SELECT id FROM items WHERE name = \'" + itemNames[i] + "\'";
+                SQLiteCommand selectCMD2 = new SQLiteCommand(selectCommand2, conn);
+                SQLiteDataReader reader2 = selectCMD2.ExecuteReader();
+                selectCMD2.Dispose();
+                if (!reader2.HasRows)
+                {
+
+                    String insertCommand2 = "INSERT INTO items (category_id, name, description, current_amount, reorder_point, max_amount) VALUES " +
+                        "(\'" + itemCategoryID[i] + "\', " +
+                        "\'" + itemNames[i] + "\', " +
+                        "\'" + itemDescription[i] + "\', " +
+                        "\'" + itemCurrAmt[i] + "\', " +
+                        "\'" + itemReorderPoint[i] + "\', " +
+                        "\'" + itemReorderAmt[i] + "\')";
+                    SQLiteCommand insertCMD2 = new SQLiteCommand(insertCommand2, conn);
+                    insertCMD2.ExecuteNonQuery();
+                    insertCMD2.Dispose();
+                }
+                else
+                {
+                    Debug.WriteLine(itemNames[i] + " already exists in the table.");
+                }
+            }
+                conn.Close();
         }
 
         // IF the item displayed is already in the DB, update the record for said item
