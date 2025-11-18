be91c92e6a5c20eb2ed9774c5dbb36541fb36b83
zxiong4 <zxiong4@student.cvtc.edu>
2025-11-17 19:32:57 -0600

    Revert "Revert "Item setup""

--- ReorderPointSystem.Tests/Models/ItemTests.cs
+++ ReorderPointSystem.Tests/Models/ItemTests.cs
@@ -1,6 +1,24 @@
-﻿namespace ReorderPointSystem.Tests.Models
+﻿using ReorderPointSystem.Data;
+using ReorderPointSystem.Models;
+
+namespace ReorderPointSystem.Tests.Models
 {
     public class ItemTests
     {
+        [Fact]
+        public void CreateAndUpdateItem()
+        {
+            Database.Initialize();
+            Item item = new Item(0, 0, "test item", "this is a test item used for testing purposes", 300, 400, 800);
+            ItemRepository repo = new ItemRepository();
+            repo.Add(item);
+            Assert.True(item.NeedsReorder());
+            item.AddStock(50);
+            Assert.True(repo.GetById(item.Id).CurrentAmount == 350);
+            item.RemoveStock(30);
+            Assert.True(repo.GetById(item.Id).CurrentAmount == 320);
+            item.UpdateStock(330);
+            Assert.True(repo.GetById(item.Id).CurrentAmount == 330);
+        }
     }
 }
