025af7d90ce7d43131ad53f23431cfeea34a11a1
Zachary Vaughn <zvaughn1@student.cvtc.edu>
2025-12-01 13:41:50 -0600

    add methods and constructor to InventoryManager

--- ReorderPointSystem/Services/InventoryManager.cs
+++ ReorderPointSystem/Services/InventoryManager.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReorderPointSystem.Data;
 
 namespace ReorderPointSystem.Services
-{
+{    
+
     internal class InventoryManager
     {
+
+        private ItemRepository _itemRepo;
+        private ReorderRepository _reorderRepo;
+        private InventoryLogRepository _inventoryLogRepo;
+
+        public InventoryManager()
+        {
+            _itemRepo = new ItemRepository();
+            _reorderRepo = new ReorderRepository();
+            _inventoryLogRepo = new InventoryLogRepository($"Data Source=rps.db;Version=3;");
+        }
+
     }
 }
